93d435b80356cc53e2889375da72e3e58a1adec4
Niklas Loos <niklas.loos@live.com>
2018-03-24 17:03:00 +0100

    Services for recurring functions

---
+++ src/Service/FilesService.php
@@ -0,0 +1,236 @@
+<?php
+/**
+ * @link https://github.com/XRayLP/learning-center-bundle
+ * @copyright Copyright (c) 2018 Niklas Loos <https://github.com/XRayLP>
+ * @license GPL-3.0 <https://github.com/XRayLP/learning-center-bundle/blob/master/LICENSE>
+ */
+
+namespace XRayLP\LearningCenterBundle\Service;
+
+
+use Contao\FilesModel;
+use Contao\FrontendUser;
+use Contao\MemberModel;
+use Doctrine\ORM\EntityManager;
+use http\Exception;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class FilesService
+{
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $router;
+
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    public function __construct(UrlGeneratorInterface $router, EntityManager $entityManager)
+    {
+        $this->router = $router;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * Creates the File Grid for the Filemanager by an Parent Folder ID and a User to check whether the User has access to the folder.
+     *
+     * @param $fid
+     * @param $objUser FrontendUser|MemberModel
+     * @return mixed
+     * @throws \Exception
+     */
+    public function createFileGrid($fid, $objUser)
+    {
+        $objFolder = $this->getFolderForFilemanager($fid, $objUser);
+
+        //has the user access to a home folder?
+        if ($objUser->assignDir && $objUser->homeDir && !$this->isFolderEmpty($objFolder))
+        {
+            $objFiles = FilesModel::findByPid($objFolder->uuid);
+
+            //process for every file in the collection
+            while ($objFiles->next()) {
+                if ($objFiles->type == 'folder') {
+                    $objFolder = new \Folder($objFiles->path);
+
+                    //url for subfolders
+                    $strHref = $this->router->generate('learningcenter_files', array('fid' => $objFiles->id));
+
+                    //adds folder to array
+                    $files[$objFolder->path] = array
+                    (
+                        'id' => $objFiles->id,
+                        'uuid' => $objFiles->uuid,
+                        'type' => $objFiles->type,
+                        'time' => date("d. M Y", $objFolder->mtime),
+                        'path' => $objFolder->path,
+                        'name' => $objFolder->filename,
+                        'href' => $strHref,
+                        'shared' => $objFiles->shared,
+                        'shared_group' => $objFiles->shared_groups
+                    );
+                } else {
+                    $objFile = new \File($objFiles->path);
+
+                    $strHref = $this->router->generate('learningcenter_files.download', array('fid' => $objFiles->id));
+
+                    //add file to array
+                    $files[$objFiles->path] = array
+                    (
+                        'id' => $objFiles->id,
+                        'uuid' => $objFiles->uuid,
+                        'time' => date("d. M Y", $objFile->mtime),
+                        'type' => $objFiles->type,
+                        'name' => $objFile->basename,
+                        'title' => \StringUtil::specialchars(sprintf($GLOBALS['TL_LANG']['MSC']['download'], $objFile->basename)),
+                        'href' => $strHref,
+                        'filesize' => '',
+                        'icon' => \Image::getPath($objFile->icon),
+                        'extension' => $objFile->extension,
+                        'path' => $objFile->dirname,
+                        'shared' => $objFiles->shared,
+                        'shared_group' => $objFiles->shared_groups
+                    );
+                }
+            }
+        }
+        return $files;
+    }
+
+    /**
+     * @param $file UploadedFile
+     * @param $fid int
+     * @param $objUser \FrontendUser|MemberModel
+     * @return string
+     * @throws \Exception
+     */
+    public function saveUploadedFile($file, $fid, $objUser)
+    {
+        $objParentFolder = $this->getFolderForFilemanager($fid, $objUser);
+        //get the path for saving the file
+        $savePath = $objParentFolder->path;
+        //original filename
+        $filename = $file->getClientOriginalName();
+
+        //moves the file from tmp to the path
+        $file->move('../'.$savePath, $filename);
+
+        $strFile = $savePath . '/' . $filename;
+
+        if (\Dbafs::shouldBeSynchronized($strFile))
+        {
+        $objModel = \FilesModel::findByPath($strFile);
+
+            if ($objModel === null)
+            {
+                $objModel = \Dbafs::addResource($strFile);
+            }
+
+            // Update the hash of the target folder
+            \Dbafs::updateFolderHashes($savePath);
+        }
+
+    }
+
+    /**
+     * @param $name string
+     * @param $fid int
+     * @param $objUser \FrontendUser|MemberModel
+     * @return string
+     * @throws \Exception
+     */
+    public function createFolder($name, $fid, $objUser)
+    {
+        $objParentFolder = $this->getFolderForFilemanager($fid, $objUser);
+        $strFile = $objParentFolder->path.'/'.$name;
+        $fs = new Filesystem();
+        //creates the folder
+        $fs->mkdir('../'.$strFile);
+
+        //creates database entry
+        if (\Dbafs::shouldBeSynchronized($strFile))
+        {
+            $objModel = \FilesModel::findByPath($strFile);
+
+            if ($objModel === null)
+            {
+                $objModel = \Dbafs::addResource($strFile);
+            }
+
+            // Update the hash of the target folder
+            \Dbafs::updateFolderHashes($objParentFolder->path);
+        }
+    }
+
+    /**
+     * deletes a file (only database entry)
+     *
+     * @param $id
+     */
+    public function deleteFile($id)
+    {
+        $strFile = FilesModel::findById($id)->path;
+        \Dbafs::deleteResource($strFile);
+    }
+
+    /**
+     * Returns the Folder Object, which should be used for the File Grid.
+     *
+     * @param $fid int
+     * @param $objUser \FrontendUser|MemberModel
+     * @return FilesModel
+     */
+    public function getFolderForFilemanager($fid, $objUser)
+    {
+        //check on integer
+        if (isset($fid) && is_numeric($fid))
+        {
+            $objFolder = FilesModel::findById($fid);
+
+            if ($objFolder->type == 'folder')
+            {
+                return $objFolder;
+            }
+        }
+
+        return FilesModel::findByUuid($objUser->homeDir);
+
+    }
+
+    /**
+     * DOESN'T WORK YET
+     *
+     * @param $objFile FilesModel
+     * @param $objUser FrontendUser|MemberModel
+     * @return bool
+     */
+    private function hasUserAccessToFileOrFolder($objFile, $objUser)
+    {
+        if (strpos($objFile->path, FilesModel::findByUuid($objUser->homeDir)->path))
+        {
+            return true;
+        }
+    }
+
+    /**
+     * Check whether a folder is empty or has subfiles/subfolders
+     *
+     * @param $objFolder
+     * @return bool
+     */
+    private function isFolderEmpty($objFolder)
+    {
+        $objFiles = FilesModel::findByPid($objFolder->uuid);
+        if ($objFiles === null) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
---
+++ src/Service/MemberService.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * @link https://github.com/XRayLP/learning-center-bundle
+ * @copyright Copyright (c) 2018 Niklas Loos <https://github.com/XRayLP>
+ * @license GPL-3.0 <https://github.com/XRayLP/learning-center-bundle/blob/master/LICENSE>
+ */
+
+namespace XRayLP\LearningCenterBundle\Service;
+
+
+use Contao\FilesModel;
+use Contao\MemberGroupModel;
+use Contao\MemberModel;
+use Contao\StringUtil;
+use LasseRafn\InitialAvatarGenerator\InitialAvatar;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class MemberService
+{
+    /**
+     * Creates an array with information about an member
+     *
+     * @param string $username
+     * @return array
+     */
+    public function getMemberDetails($username)
+    {
+        $objMember = MemberModel::findByUsername($username);
+        if (isset($objMember))
+        {
+            $avatarImage = $this->getAvatar($objMember);
+            $member = array(
+                'id'        => $objMember->id,
+                'username'  => $objMember->username,
+                'firstname' => $objMember->firstname,
+                'lastname'  => $objMember->lastname,
+                'email'     => $objMember->email,
+                'gender'    => $objMember->gender,
+                'avatar'    => $avatarImage
+            );
+            return $member;
+        } else {
+            throw new NotFoundHttpException("Member with the username '$username' doesn't exist");
+        }
+    }
+
+    /**
+     * Creates a list of members from a collection
+     *
+     * @param $collection
+     * @return mixed
+     */
+    public function getMemberList($collection)
+    {
+        foreach ($collection as $objMember)
+        {
+            $members[$objMember->firstname.' '.$objMember->lastname] =$objMember->id;
+        }
+
+        return $members;
+    }
+
+    /**
+     * Creates a profile image for the user with the first latter of first and last name
+     *
+     * @param \MemberModel $objUser
+     */
+    public function createAvatar($objUser)
+    {
+        $avatar = new InitialAvatar();
+        $image = $avatar
+            ->name("$objUser->firstname $objUser->lastname")
+            ->background($this->getGenderColor($objUser->gender))
+            ->color('#fff')
+            ->size(100)
+            ->generate();
+        $image->save("bundles/learningcenter/avatar/$objUser->id.png");
+
+        //High Quality Image
+        $image = $avatar
+            ->name("$objUser->firstname $objUser->lastname")
+            ->background($this->getGenderColor($objUser->gender))
+            ->color('#fff')
+            ->size(1000)
+            ->generate();
+        $image->save("bundles/learningcenter/avatar/".$objUser->id."_hq.png");
+        $objUser->avatar = 1;
+        $objUser->save();
+    }
+
+    /**
+     * Returns the Avatar Path from a user
+     *
+     * @param \MemberModel $objMember
+     * @param bool $highQuality
+     * @return string
+     */
+    public function getAvatar($objMember, $highQuality = false)
+    {
+        if ($objMember->avatar == 0)
+        {
+            $this->createAvatar($objMember);
+        }
+        return "/bundles/learningcenter/avatar/".$objMember->id."_hq.png";
+    }
+
+    /**
+     * Adds a User to a specific group
+     *
+     * @param MemberModel $objMember
+     * @param MemberGroupModel $objGroup
+     */
+    public function addMemberToGroup($objMember, $objGroup)
+    {
+        $arrGroups = StringUtil::deserialize($objMember->groups);
+        array_push($arrGroups, $objGroup->id);
+        $objMember->groups = serialize($arrGroups);
+        $objMember->save();
+    }
+
+    /**
+     * returns a color code based on the user's gender
+     *
+     * @param string $gender
+     * @return string
+     */
+    public function getGenderColor($gender)
+    {
+        switch ($gender)
+        {
+            case 'male':
+                $color = '#2E64FE';
+                break;
+
+            case 'female':
+                $color = '#DF01D7';
+                break;
+
+            default:
+                $color = '#3ADF00';
+                break;
+        }
+
+        return $color;
+    }
+}
