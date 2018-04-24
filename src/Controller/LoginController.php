6ab148c11e00095dd65355e3c2641fbc57b3cfb8
Niklas Loos <niklas.loos@live.com>
2018-04-24 19:20:11 +0200

    show login errors

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,11 +7,11 @@
 
 namespace XRayLP\LearningCenterBundle\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
-use System;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\LockedException;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class LoginController extends Controller
 {
@@ -20,13 +20,34 @@ class LoginController extends Controller
         //Check if the User isn't granted
         if (!$this->get('security.authorization_checker')->isGranted('ROLE_MEMBER'))
         {
+            //Exception Variables
+            $exception = $this->get('security.authentication_utils')->getLastAuthenticationError();
+            $hasError = false;
+            $message = '';
+
+            //Get Exceptions
+            if ($exception instanceof LockedException)
+            {
+                $hasError = true;
+                $message = $this->get('translator')->trans('This account has been locked! You can log in again in %d minutes.', $exception->getLockedMinutes());
+            }
+            elseif ($exception instanceof AuthenticationException)
+            {
+                $hasError = true;
+                $message = $this->get('translator')->trans('Login failed (note that usernames and passwords are case-sensitive)!');
+            }
+
             //Twig
             $twigRenderer = $this->get('templating');
-            $rendered = $twigRenderer->render('@LearningCenter/security/login.html.twig');
+            $rendered = $twigRenderer->render('@LearningCenter/security/login.html.twig', array(
+                'hasError'  => $hasError,
+                'message'   => $message
+            ));
             return new Response($rendered);
 
         } else {
-            return $this->redirectToRoute('learningcenter');        }
+            return $this->redirectToRoute('learningcenter');
+        }
     }
 
     public function logoutAction()
