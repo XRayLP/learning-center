c18380d54884ebf3476dd90730926f19638d6ea9
Niklas Loos <niklas.loos@live.com>
2018-07-27 11:02:58 +0200

    foundation for an own authentication service

---
+++ src/Listener/LoginListener.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @link https://github.com/XRayLP/learning-center-bundle
+ * @copyright Copyright (c) 2018 Niklas Loos <https://github.com/XRayLP>
+ * @license GPL-3.0 <https://github.com/XRayLP/learning-center-bundle/blob/master/LICENSE>
+ */
+
+namespace XRayLP\LearningCenterBundle\Listener;
+
+use Symfony\Component\Security\Core\Event\AuthenticationFailureEvent;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+class LoginListener
+{
+
+
+    /**
+     * onAuthenticationFailure
+     *
+     * @author 	Joe Sexton <joe@webtipblog.com>
+     * @param 	AuthenticationFailureEvent $event
+     */
+    public function onAuthenticationFailure( AuthenticationFailureEvent $event )
+    {
+        // executes on failed login
+    }
+
+    /**
+     * onAuthenticationSuccess
+     *
+     * @author 	Joe Sexton <joe@webtipblog.com>
+     * @param 	InteractiveLoginEvent $event
+     */
+    public function onAuthenticationSuccess( InteractiveLoginEvent $event )
+    {
+        //$event->getAuthenticationToken()->getUser()->setRoles(array('ROLE_MEMBER', 'ROLE_TEACHER'));
+    }
+}
