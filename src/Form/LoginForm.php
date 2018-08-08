7987b10f64e18cb368d9035fd5e254d0471790e3
Niklas Loos <niklas.loos@live.com>
2018-08-08 12:30:47 +0200

    try to add another authenticator (doesn't work, because of contao)

---
+++ src/Form/LoginForm.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @link https://github.com/XRayLP/learning-center-bundle
+ * @copyright Copyright (c) 2018 Niklas Loos <https://github.com/XRayLP>
+ * @license GPL-3.0 <https://github.com/XRayLP/learning-center-bundle/blob/master/LICENSE>
+ */
+
+namespace XRayLP\LearningCenterBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class LoginForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('_username')
+            ->add('_password', PasswordType::class)
+        ;
+    }
+
+}
---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * @link https://github.com/XRayLP/learning-center-bundle
+ * @copyright Copyright (c) 2018 Niklas Loos <https://github.com/XRayLP>
+ * @license GPL-3.0 <https://github.com/XRayLP/learning-center-bundle/blob/master/LICENSE>
+ */
+
+namespace XRayLP\LearningCenterBundle\Security;
+
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\EncoderAwareInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use XRayLP\LearningCenterBundle\Entity\Member;
+use XRayLP\LearningCenterBundle\Form\LoginForm;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+{
+
+    protected $formFactory;
+
+    protected $entityManager;
+
+    protected $router;
+
+    public function __construct(FormFactoryInterface $formFactory, EntityManager $entityManager, RouterInterface $router)
+    {
+        $this->formFactory = $formFactory;
+
+        $this->entityManager = $entityManager;
+
+        $this->router = $router;
+
+    }
+
+    /**
+     * Return the URL to the login page.
+     *
+     * @return string
+     */
+    protected function getLoginUrl()
+    {
+        return $this->router->generate('learningcenter_security.login');
+    }
+
+    /**
+     * Get the authentication credentials from the request and return them
+     * as any type (e.g. an associate array).
+     *
+     * Whatever value you return here will be passed to getUser() and checkCredentials()
+     *
+     * For example, for a form login, you might:
+     *
+     *      return array(
+     *          'username' => $request->request->get('_username'),
+     *          'password' => $request->request->get('_password'),
+     *      );
+     *
+     * Or for an API token that's on a header, you might use:
+     *
+     *      return array('api_key' => $request->headers->get('X-API-TOKEN'));
+     *
+     * @param Request $request
+     *
+     * @return mixed Any non-null value
+     *
+     * @throws \UnexpectedValueException If null is returned
+     */
+    public function getCredentials(Request $request)
+    {
+        $isLoginSubmit = $request->attributes->get('_route') === 'learningcenter_security.login' && $request->isMethod('POST');
+
+        if (!$isLoginSubmit) {
+            // skip authentication
+            return;
+        }
+        $form = $this->formFactory->create(LoginForm::class);
+        $form->handleRequest($request);
+        $data = $form->getData();
+        return $data;
+    }
+
+    /**
+     * Return a UserInterface object based on the credentials.
+     *
+     * The *credentials* are the return value from getCredentials()
+     *
+     * You may throw an AuthenticationException if you wish. If you return
+     * null, then a UsernameNotFoundException is thrown for you.
+     *
+     * @param mixed $credentials
+     * @param UserProviderInterface $userProvider
+     *
+     * @throws AuthenticationException
+     *
+     * @return UserInterface|null
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $username = $credentials['_username'];
+
+        return $this->entityManager->getRepository(Member::class)->findOneBy(['username' => $username]);
+    }
+
+    /**
+     * Returns true if the credentials are valid.
+     *
+     * If any value other than true is returned, authentication will
+     * fail. You may also throw an AuthenticationException if you wish
+     * to cause authentication to fail.
+     *
+     * The *credentials* are the return value from getCredentials()
+     *
+     * @param mixed $credentials
+     * @param UserInterface $user
+     *
+     * @return bool
+     *
+     * @throws AuthenticationException
+     */
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        $password = $credentials['_password'];
+
+        if (password_verify($password, $user->getPassword()))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
